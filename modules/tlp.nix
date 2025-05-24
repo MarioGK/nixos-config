dccaf84ffac9b2a20367a60d5440068d4b2ea831
MarioGK <mariogk01@gmail.com>
2025-05-24 14:45:09 +0100

    Fixing tlp settings

--- modules/tlp.nix
+++ modules/tlp.nix
@@ -4,5 +4,5 @@
   # Disable power-profiles-daemon as it conflicts with TLP
   services.power-profiles-daemon.enable = false;
   environment.systemPackages = with pkgs; [ tlp tlpui ];
-  services.tlp.extraConfig = builtins.readFile ../files/tlp.conf;
+  config.services.tlp.settings = builtins.readFile ../files/tlp.conf;
 }
