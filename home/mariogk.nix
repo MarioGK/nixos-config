2924efc4909bfb40dc4c3d69160e4904681be12c
Mario Gabriell Karaziaki <MarioGK@users.noreply.github.com>
2025-05-24 18:16:49 +0100

    Fix missing home stateVersion

--- home/mariogk.nix
+++ home/mariogk.nix
@@ -11,6 +11,9 @@
 
   home.username = "mariogk";
   home.homeDirectory = "/home/mariogk";
+  # Set the version of Home Manager used for this user's configuration.
+  # This value should not be changed once initially set.
+  home.stateVersion = "24.05";
 
   programs.home-manager.enable = true;
 
