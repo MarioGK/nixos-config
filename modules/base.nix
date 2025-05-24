85aff1ad33185e2b2645ce3140c298d1246c03a1
Mario Gabriell Karaziaki <MarioGK@users.noreply.github.com>
2025-05-24 18:40:33 +0100

    Add pipewire library for Qt

--- modules/base.nix
+++ modules/base.nix
@@ -185,6 +185,8 @@ in
     dotnetCombined
     aspnetCombined
     inputs.zen-browser.packages.${pkgs.system}.default
+    # Provide libpipewire for Qt multimedia
+    pkgs.pipewire
   ];
 
   # Ensure Aspire workload is available with the installed .NET SDKs
